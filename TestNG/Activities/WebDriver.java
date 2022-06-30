443145af5a11fea14f54765d414ad933a99cd94d
sasikumarsamuel <sasisamuel@in.ibm.com>
2022-06-30 12:46:02 +0530

    Add files via upload

---
+++ TestNG/Activities/WebDriver.java
@@ -0,0 +1,26 @@
+package Activities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class WebDriver {
+    public void get(String s) {
+    }
+
+    public String getTitle() {
+        return null;
+    }
+
+    public WebElement findElement(By id) {
+        return null;
+    }
+
+
+    public void quit() {
+    }
+    public org.openqa.selenium.WebDriver.TargetLocator switchTo() {
+        return null;
+    }
+    public void close() {
+    }
+}
